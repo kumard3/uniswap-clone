7b40c2a19f8a4c26860731e30555cceabf30e235
kumard3 <kumardeepanshu157200@gmail.com>
2022-02-27 22:39:27 +0530

    TransactionHistory tab added

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,16 +1,20 @@
+import type { NextPage } from "next";
 import Header from "../components/Header";
 import Main from "../components/Main";
+import TransactionHistory from "../components/TransactionHistory";
 
-const styles = {
-  wrapper: `h-screen max-h-screen h-min-screen w-screen bg-[#2d242f] text-white select-none flex flex-col justify-between`,
+const style = {
+  wrapper: `h-screen max-h-screen h-min-screen w-screen bg-[#2D242F] text-white select-none flex flex-col justify-between`,
 };
 
-export default function Home() {
+const Home: NextPage = () => {
   return (
-    <div className={styles.wrapper}>
+    <div className={style.wrapper}>
       <Header />
       <Main />
-      <h2>Transactions History</h2>
+      <TransactionHistory />
     </div>
   );
-}
+};
+
+export default Home;
