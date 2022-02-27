2b2c8c9bcbd867fa0b332dbd36f41192d6d6d230
kumard3 <kumardeepanshu157200@gmail.com>
2022-02-28 00:23:11 +0530

    new style added

--- client/components/Main.tsx
+++ client/components/Main.tsx
@@ -1,20 +1,20 @@
-import Image from 'next/image'
-import { RiSettings3Fill } from 'react-icons/ri'
-import { AiOutlineDown } from 'react-icons/ai'
-import ethLogo from '../assets/eth.png'
-import { useContext } from 'react'
-import { TransactionContext } from '../context/TransactionContext'
-import Modal from 'react-modal'
-import { useRouter } from 'next/router'
-import TransactionLoader from './TransactionLoader'
+import Image from "next/image";
+import { RiSettings3Fill } from "react-icons/ri";
+import { AiOutlineDown } from "react-icons/ai";
+import ethLogo from "../assets/eth.png";
+import { useContext } from "react";
+import { TransactionContext } from "../context/TransactionContext";
+import Modal from "react-modal";
+import { useRouter } from "next/router";
+import TransactionLoader from "./TransactionLoader";
 
-Modal.setAppElement('#__next')
+Modal.setAppElement("#__next");
 
 const style = {
-  wrapper: `w-screen flex items-center justify-center mt-14`,
-  content: `bg-[#191B1F] w-[40rem] rounded-2xl p-4`,
+  wrapper: `w-screen flex items-center justify-center mt-14 px-3`,
+  content: ` bg-black/80 w-[40rem] backdrop-blur-md	 rounded-2xl p-4`,
   formHeader: `px-2 flex items-center justify-between font-semibold text-xl`,
-  transferPropContainer: `bg-[#20242A] my-3 rounded-2xl p-6 text-3xl  border border-[#20242A] hover:border-[#41444F]  flex justify-between`,
+  transferPropContainer: `bg-slate-900	  my-3 rounded-2xl p-6 text-3xl  border border-gray-800 hover:border-[#41444F]  flex justify-between`,
   transferPropInput: `bg-transparent placeholder:text-[#B2B9D2] outline-none mb-6 w-full text-2xl`,
   currencySelector: `flex w-1/4`,
   currencySelectorContent: `w-full h-min flex justify-between items-center bg-[#2D2F36] hover:bg-[#41444F] rounded-2xl text-xl font-medium cursor-pointer p-2 mt-[-0.2rem]`,
@@ -22,37 +22,37 @@ const style = {
   currencySelectorTicker: `mx-2`,
   currencySelectorArrow: `text-lg`,
   confirmButton: `bg-[#2172E5] my-2 rounded-2xl py-6 px-8 text-xl font-semibold flex items-center justify-center cursor-pointer border border-[#2172E5] hover:border-[#234169]`,
-}
+};
 
 const customStyles = {
   content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    transform: 'translate(-50%, -50%)',
-    backgroundColor: '#0a0b0d',
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    transform: "translate(-50%, -50%)",
+    backgroundColor: "#0a0b0d",
     padding: 0,
-    border: 'none',
+    border: "none",
   },
   overlay: {
-    backgroundColor: 'rgba(10, 11, 13, 0.75)',
+    backgroundColor: "rgba(10, 11, 13, 0.75)",
   },
-}
+};
 
 const Main = () => {
   const { formData, handleChange, sendTransaction } =
-    useContext(TransactionContext)
-  const router = useRouter()
+    useContext(TransactionContext);
+  const router = useRouter();
 
   const handleSubmit = async (e: any) => {
-    const { addressTo, amount } = formData
-    e.preventDefault()
+    const { addressTo, amount } = formData;
+    e.preventDefault();
 
-    if (!addressTo || !amount) return
+    if (!addressTo || !amount) return;
 
-    sendTransaction()
-  }
+    sendTransaction();
+  };
 
   return (
     <div className={style.wrapper}>
@@ -65,16 +65,16 @@ const Main = () => {
         </div>
         <div className={style.transferPropContainer}>
           <input
-            type='text'
+            type="text"
             className={style.transferPropInput}
-            placeholder='0.0'
-            pattern='^[0-9]*[.,]?[0-9]*$'
-            onChange={e => handleChange(e, 'amount')}
+            placeholder="0.0"
+            pattern="^[0-9]*[.,]?[0-9]*$"
+            onChange={(e) => handleChange(e, "amount")}
           />
           <div className={style.currencySelector}>
             <div className={style.currencySelectorContent}>
               <div className={style.currencySelectorIcon}>
-                <Image src={ethLogo} alt='eth logo' height={20} width={20} />
+                <Image src={ethLogo} alt="eth logo" height={20} width={20} />
               </div>
               <div className={style.currencySelectorTicker}>ETH</div>
               <AiOutlineDown className={style.currencySelectorArrow} />
@@ -83,14 +83,14 @@ const Main = () => {
         </div>
         <div className={style.transferPropContainer}>
           <input
-            type='text'
+            type="text"
             className={style.transferPropInput}
-            placeholder='0x...'
-            onChange={e => handleChange(e, 'addressTo')}
+            placeholder="0x..."
+            onChange={(e) => handleChange(e, "addressTo")}
           />
           <div className={style.currencySelector}></div>
         </div>
-        <div onClick={e => handleSubmit(e)} className={style.confirmButton}>
+        <div onClick={(e) => handleSubmit(e)} className={style.confirmButton}>
           Confirm
         </div>
       </div>
@@ -99,7 +99,7 @@ const Main = () => {
         <TransactionLoader />
       </Modal>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -4,17 +4,31 @@ import Main from "../components/Main";
 import TransactionHistory from "../components/TransactionHistory";
 
 const style = {
-  wrapper: `h-screen max-h-screen h-min-screen w-screen bg-[#2D242F] text-white select-none flex flex-col justify-between`,
+  wrapper: `h-screen  max-h-screenh-screen  max-h-screen -z-10  h-min-screen w-screen bg-[url('/test.jpg')] bg-no-repeat bg-cover	 bg-center 	 ob text-white select-none flex flex-col justify-between`,
+  wrapper2: `h-screen  max-h-screenh-screen `,
 };
 
 const Home: NextPage = () => {
   return (
     <div className={style.wrapper}>
-      <Header />
-      <Main />
-      <TransactionHistory />
+      <div className=" after:content-[''] after:absolute after:w-full after:h-full  after:backdrop-blur-sm"></div>
+      <div className={`${style.wrapper2} z-10`}>
+        <Header />
+        <Main />
+        <TransactionHistory />
+      </div>
     </div>
   );
 };
 
 export default Home;
+/* 
+
+
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center center;
+    background: blur;
+    backdrop-filter: blur(1038px);
+
+*/
