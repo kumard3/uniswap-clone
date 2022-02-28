b23606838aba523725bd108be170c8dc07544e19
Kumar Deepanshu <62591080+kumard3@users.noreply.github.com>
2022-02-28 14:56:52 +0530

    Update index.tsx

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -4,15 +4,15 @@ import Main from "../components/Main";
 import TransactionHistory from "../components/TransactionHistory";
 
 const style = {
-  wrapper: `h-screen  max-h-screenh-screen  max-h-screen -z-10  h-min-screen w-screen bg-[url('/test.jpg')] bg-no-repeat bg-cover	 bg-center 	 ob text-white select-none flex flex-col justify-between`,
-  wrapper2: `h-screen  max-h-screenh-screen `,
+  wrapper: `h-screen    max-h-screen -z-10   w-screen bg-[url('/test.jpg')] bg-no-repeat bg-cover	 bg-center  text-white select-none flex flex-col justify-between`,
+  wrapper2: `h-screen   `,
 };
 
 const Home: NextPage = () => {
   return (
     <div className={style.wrapper}>
       <div className=" after:content-[''] after:absolute after:w-full after:h-full  after:backdrop-blur-sm"></div>
-      <div className={`${style.wrapper2} z-10`}>
+      <div className={`${style.wrapper2} `}>
         <Header />
         <Main />
         <TransactionHistory />
