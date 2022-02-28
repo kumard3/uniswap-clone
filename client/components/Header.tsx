c323106885e79852747c30888b01143d4eef6ac7
Kumar Deepanshu <62591080+kumard3@users.noreply.github.com>
2022-02-28 14:57:39 +0530

    Update Header.tsx

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -8,18 +8,18 @@ import uniswapLogo from "../assets/uniswap.png";
 import { TransactionContext } from "../context/TransactionContext";
 
 const style = {
-  wrapper: `p-4 w-screen flex justify-between items-center`,
-  headerLogo: `flex  items-center justify-start`,
-  nav: ` flex justify-between items-center`,
-  navItemsContainer: `flex bg-[#191B1F] rounded-3xl`,
-  navItem: `px-4 py-2 m-1 flex items-center text-lg font-semibold text-[0.9rem] cursor-pointer rounded-3xl`,
+  wrapper: `p-4 w-screen flex justify-between items-center z-10`,
+  headerLogo: `flex z-10  items-center justify-start`,
+  nav: ` flex z-10 justify-between items-center`,
+  navItemsContainer: `flex bg-[#191B1F] z-10 rounded-3xl`,
+  navItem: `px-4 py-2 m-1 flex items-center z-10 text-lg font-semibold text-[0.9rem] cursor-pointer rounded-3xl`,
   activeNavItem: `bg-[#20242A]`,
-  buttonsContainer: `flex justify-end items-center`,
-  button: `flex items-center bg-[#191B1F] rounded-2xl mx-2 text-[0.9rem] font-semibold cursor-pointer`,
+  buttonsContainer: `flex justify-end items-center z-10`,
+  button: `flex items-center bg-[#191B1F] rounded-2xl z-10 mx-2 text-[0.9rem] font-semibold cursor-pointer`,
   buttonPadding: `p-2`,
-  buttonTextContainer: `h-8 flex items-center`,
-  buttonIconContainer: `flex items-center justify-center w-8 h-8`,
-  buttonAccent: `bg-[#172A42] border border-[#163256] hover:border-[#234169] h-full rounded-2xl flex items-center justify-center text-[#4F90EA]`,
+  buttonTextContainer: `h-8 flex items-center z-10`,
+  buttonIconContainer: `flex items-center justify-center w-8 h-8 z-10`,
+  buttonAccent: `bg-[#172A42] border border-[#163256] z-10 hover:border-[#234169] h-full rounded-2xl flex items-center justify-center text-[#4F90EA]`,
 };
 
 const Header = () => {
